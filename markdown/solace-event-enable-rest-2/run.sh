6208dba6877006c306cbefabf3a5197d48299925
Aaron Lee <github@aaronlee.ca>
2021-01-31 22:45:18 +0800

    updates

---
+++ markdown/solace-event-enable-rest-2/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# little helper script so I don't have to keep typing these same commands over and over!
+
+vim solace-event-enable-rest.md
+claat export -ga UA-3921398-10 solace-event-enable-rest-2.md
+claat serve
